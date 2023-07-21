063436fe19c7de92bf26f1cb0c54039c8eafd3b2
mikeoo1 <micthomas407@gmail.com>
2023-07-21 12:15:43 +0100

    newly-pushed

--- Middleware/authorization.js
+++ Middleware/authorization.js
@@ -1,58 +1,58 @@
-const jwt = require("jsonwebtoken");
-const User = require("../Models/model");
+const jwt = require("jsonwebtoken")
+const User = require("../Models/model")
 
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require("dotenv")
+dotenv.config()
 
 // auth middleware
 const userAuth = async (req, res, next) => {
   try {
     if (req.headers.authorization) {
         const hasAuthorization = req.headers.authorization
-      const token = hasAuthorization.split(" ")[1];
+      const token = hasAuthorization.split(" ")[1]
 
       const decodedToken = await jwt.verify(token, process.env.JWT_TOKEN);
-      console.log(decodedToken);
-      req.user = JSON.stringify(decodedToken);
-      req.userId = decodedToken.userId;
+      console.log(decodedToken)
+      req.user = JSON.stringify(decodedToken)
+      req.userId = decodedToken.userId
      
-      next();
+      next()
     } else {
       res.status(404).json({
         message: "No authorization found",
-      });
+      })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 const isAdminAuthorized = async (req, res, next) => {
   try {
-console.log(req.userId);
-    const user = await User.findById(req.userId);
-    console.log(user);
+console.log(req.userId)
+    const user = await User.findById(req.userId)
+    console.log(user)
     if (user.isAdmin) {
-      next();
+      next()
     } else {
-      res.status(401).json({ message: "not an admin" });
+      res.status(401).json({ message: "not an admin" })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 const isSuperAdminAuthorized = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
     if (user.isSuperAdmin) {
-      next();
+      next()
     } else {
-      res.status(401).json({ message: "not a super admin" });
+      res.status(401).json({ message: "not a super admin" })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
-module.exports = { userAuth, isAdminAuthorized, isSuperAdminAuthorized};
+module.exports = { userAuth, isAdminAuthorized, isSuperAdminAuthorized}
